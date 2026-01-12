1a4c3f6f091c1146afec7efa7cc1c4ea38d2e0b8
Nat <natalie.tay@discourse.org>
2026-01-12 21:13:44 +0800

    spec updates

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -355,8 +355,8 @@ RSpec.describe Tag do
   describe "full_url" do
     let(:tag) { Fabricate(:tag, name: "🚀") }
 
-    it "percent encodes emojis" do
-      expect(tag.full_url).to eq("http://test.localhost/tag/%F0%9F%9A%80")
+    it "uses slug/id format" do
+      expect(tag.full_url).to eq("http://test.localhost/tag/#{tag.id}-tag/#{tag.id}")
     end
   end
 
